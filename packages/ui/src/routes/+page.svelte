b136106582fcb53e4dcdc3d7605c4ae0c2830f24
afaqjaved <afaqjavedofficial@gmail.com>
2026-05-14 22:19:54 +0500

    commited shared package

--- packages/ui/src/routes/+page.svelte
+++ packages/ui/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
 	import Button from '$lib/components/ui/button/button.svelte';
+	import { sum } from '@pikslots/shared';
 </script>
 
-<h1 class="bg-red-400">Welcome to SvelteKit</h1>
+<h1 class="bg-red-400">Welcome to SvelteKit {sum()}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <Button variant={'outline'}>ClickME</Button>
