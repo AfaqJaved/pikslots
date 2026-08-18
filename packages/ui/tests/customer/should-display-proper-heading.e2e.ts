2e4ebd0947b5c55d470da2e19a0f2d0b9f600c3a
amirali729 <amirpechs10@gmail.com>
2026-08-18 13:36:00 +0500

    fix(tests): format code in should-display-proper-heading test

--- packages/ui/tests/customer/should-display-proper-heading.e2e.ts
+++ packages/ui/tests/customer/should-display-proper-heading.e2e.ts
@@ -1,10 +1,9 @@
 import test, { expect } from '@playwright/test';
 
 test('customer page should have proper heading', async ({ page }) => {
-    await page.goto('/home/customers');
+	await page.goto('/home/customers');
 
-    const heading = page.getByTestId('customer-heading');
+	const heading = page.getByTestId('customer-heading');
 
-    await expect(heading).toHaveText('Customers');
-    
+	await expect(heading).toHaveText('Customers');
 });
