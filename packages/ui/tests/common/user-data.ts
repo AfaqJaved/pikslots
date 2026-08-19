2a423a9d1e9b1faf64780b937f2f99947576d3c7
amirali729 <amirpechs10@gmail.com>
2026-08-19 13:07:49 +0500

    format the code

--- packages/ui/tests/common/user-data.ts
+++ packages/ui/tests/common/user-data.ts
@@ -7,28 +7,28 @@
 // -- =============================================================================
 
 export const USER = {
-    PLATFORM_OWNER: {
-        username: "afaqjaved",
-        password: "admin12345"
-    },
-    BUSINESS_OWNER: {
-        username: "businessowner",
-        password: "admin12345"
-    },
-    ADMIN: {
-        username: "admin",
-        password: "admin12345"
-    },
-    STANDARD: {
-        username: "standard",
-        password: "admin12345"
-    },
-    ENHANCED: {
-        username: "enhanced",
-        password: "admin12345"
-    },
-    NO_ACCESS: {
-        username: "noaccess",
-        password: "noaccess"
-    }
-}
+	PLATFORM_OWNER: {
+		username: 'afaqjaved',
+		password: 'admin12345'
+	},
+	BUSINESS_OWNER: {
+		username: 'businessowner',
+		password: 'admin12345'
+	},
+	ADMIN: {
+		username: 'admin',
+		password: 'admin12345'
+	},
+	STANDARD: {
+		username: 'standard',
+		password: 'admin12345'
+	},
+	ENHANCED: {
+		username: 'enhanced',
+		password: 'admin12345'
+	},
+	NO_ACCESS: {
+		username: 'noaccess',
+		password: 'noaccess'
+	}
+};
--- packages/ui/tests/login/should-display-error-toast-for-invalid-credentials.e2e.ts
+++ packages/ui/tests/login/should-display-error-toast-for-invalid-credentials.e2e.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test';
-import { USER } from '../common/user-data'
+import { USER } from '../common/user-data';
 
 test('should display error toast when credentials are incorrect', async ({ page }) => {
 	await page.goto('/login');
@@ -7,7 +7,7 @@ test('should display error toast when credentials are incorrect', async ({ page 
 	const email = page.getByTestId('email');
 	const password = page.getByTestId('password');
 	const loginBtn = page.getByTestId('login-btn');
-	
+
 	await email.fill(USER.NO_ACCESS.username);
 	await password.fill(USER.NO_ACCESS.password);
 
