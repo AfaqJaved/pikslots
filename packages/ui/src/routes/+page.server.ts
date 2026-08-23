a54ea73a2ab7f7d20825596f1f25838e30250299
afaqjaved <afaqjavedofficial@gmail.com>
2026-08-23 14:35:19 +0500

    commited in the same pr

--- packages/ui/src/routes/+page.server.ts
+++ packages/ui/src/routes/+page.server.ts
@@ -3,6 +3,9 @@ import { getBusinessSlugFromHost, isAppHost } from '$utils/tenant-host';
 export const load = ({ url }) => {
 	const host = url.hostname;
 
+	console.log(host);
+	console.log(getBusinessSlugFromHost(host));
+
 	return {
 		businessSlug: getBusinessSlugFromHost(host),
 		isAppHost: isAppHost(host)
